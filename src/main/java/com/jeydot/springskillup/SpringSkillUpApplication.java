f50d85cae71b3cfb7d7926c3e5c75f6a225743ec
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2025-06-18 09:59:02 +0000

    Initial spring boot commit

---
+++ src/main/java/com/jeydot/springskillup/SpringSkillUpApplication.java
@@ -0,0 +1,13 @@
+package com.jeydot.springskillup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSkillUpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSkillUpApplication.class, args);
+    }
+
+}
